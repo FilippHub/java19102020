9ae8489ffba139bba578469fb3ac98ffcf3dee67
Philip <filya4565@gmail.com>
2020-11-04 21:10:55 +0300

    hw4.1

--- src/lesson4/Main.java
+++ src/lesson4/Main.java
@@ -17,3 +17,30 @@ public class Main {
         }
     }
 }
+public class Person {
+    private String fio;
+    private String position;
+    private String email;
+    private int phone;
+    private int salary;
+    private int age;
+
+    public Person(String fio, String position, String email, int phone, int salary, int age) {
+        this.fio = fio;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void getFullInfo() {
+        System.out.println("fio:" + fio + ", position: " + position
+                + ", email: " + email + ", salary: " + salary + ", age: "
+                + age);
+    }
+
+    int getAge() {
+        return age;
+    }
+}
